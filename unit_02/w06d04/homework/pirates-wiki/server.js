5ae1797d5313359f996170f87bafa843139cd2fa
Magkinney21 <magkinney21@gmail.com>
2017-03-17 00:46:43 -0400

    the crew

--- unit_02/w06d04/homework/pirates-wiki/server.js
+++ unit_02/w06d04/homework/pirates-wiki/server.js
@@ -1,1 +1,30 @@
 //your code here
+var express = require('express');
+var app     = express();
+var port    = 3000;
+
+var port = process.env.PORT || 3000;
+
+var bodyParser = require('body-parser');
+
+
+app.use(express.static(__dirname + '/public/css'));
+
+app.set("view engine", "hbs");
+
+//controllers for `/pirates` resource
+var pirateController = require('./controllers/pirates.js');
+
+app.use("/pirates", pirateController);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.send('homepage');
+});
+
+app.listen(port, function(){
+  console.log("===========================")
+  console.log('Andre' + port);
+  console.log("===========================")
+});
