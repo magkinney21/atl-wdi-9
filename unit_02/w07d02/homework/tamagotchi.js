768f405fad5bf158850da72f6acde17a577a2f06
Magkinney21 <magkinney21@gmail.com>
2017-03-20 23:35:10 -0400

    added new methods

--- unit_02/w07d02/homework/tamagotchi.js
+++ unit_02/w07d02/homework/tamagotchi.js
@@ -7,18 +7,29 @@ var Tamagotchis = function(name, creatureType) {
   this.foodInTummy = 10;
   this.restedness = 10;
   this.health = 10 ;
-  this.cry = Tamagotchis.reduce(function(accumulator, v) {
+  this.cry = Tamagotchis.foodInTummy.reduce(function(accumulator, v) {
   return accumulator - v;}, 10);
-  console.log('WAHHH', + 'food in tummy', sum);
+  console.log(this.name + 'WAHHH', + 'has this much food in tummy = ', sum);
 
 };
+  this.puke = Tamagotchis.health.reduce(function(accumulator, v) {
+  return accumulator - v;}, 10);
+  console.log(this.name + ' has current health = ', +   sum);
 
+};
+this.yawn = Tamagotchis.restedness.reduce(function(accumulator, v) {
+  return accumulator - v;}, 10);
+  console.log(this.name + ' has current restedness of = ',+  sum);
+
+};
 
 
 
 //create new Tamagotchis
 var oneTamagotchi = new Tamagotchi("Cactus Angel", "Cactus");
 var twoTamagotchi = new Tamagotchi("Babytchi", "ShiroBabytchi");
+var oneTamagotchi = new Tamagotchi("Mametchi", "Mame");
+var twoTamagotchi = new Tamagotchi("Kuchipatchi", "Kuchi");
 
 //test out your Tamagotchies below via console.logs
 console.log(oneTamagotchi);
