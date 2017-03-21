cc82e3bd25e1ea19b0a40f48bd63b1bfa859fd84
Magkinney21 <magkinney21@gmail.com>
2017-03-20 23:08:49 -0400

    added arguments to Tamagotchi

--- unit_02/w07d02/homework/tamagotchi.js
+++ unit_02/w07d02/homework/tamagotchi.js
@@ -7,21 +7,23 @@ var Tamagotchis = function(name, creatureType) {
   this.foodInTummy = 10;
   this.restedness = 10;
   this.health = 10 ;
-
   this.cry = Tamagotchis.reduce(function(accumulator, v) {
   return accumulator - v;}, 10);
   console.log('WAHHH', + 'food in tummy', sum);
 
 };
-var oneTamagotchi = new Tamagotchi();
-var twoTamagotchi = new Tamagotchi();
 
-oneTamagotchi.cry();
-twoTamagotchi.cry();
 
 
 
 //create new Tamagotchis
-
+var oneTamagotchi = new Tamagotchi("Cactus Angel", "Cactus");
+var twoTamagotchi = new Tamagotchi("Babytchi", "ShiroBabytchi");
 
 //test out your Tamagotchies below via console.logs
+console.log(oneTamagotchi);
+console.log(twoTamagotchi);
+
+//Invoke methods on the constructed objects:
+oneTamagotchi.cry();
+twoTamagotchi.cry();
