4e656edb51b238ca12d5099600fd89cdc4c5bdfd
Magkinney21 <magkinney21@gmail.com>
2017-04-03 22:04:29 -0400

    done

--- unit_03/w09d04/student_labs/criminals-starter-code-single-app/public/js/criminalsController.js
+++ unit_03/w09d04/student_labs/criminals-starter-code-single-app/public/js/criminalsController.js
@@ -8,6 +8,8 @@ function CriminalsController($http){
   vm.all = [];
   vm.addCriminal = addCriminal;
   vm.newCriminal = {};
+  vm.getCriminals = getCriminals;
+  vm.deleteCriminal = deleteCriminal;
 
   getCriminals();
   function getCriminals(){
@@ -15,7 +17,31 @@ function CriminalsController($http){
       .get('/criminals')
       .then(function(response){
         vm.all = response.data.criminals;
-      });
-    }
+    });
+  }
+  function addCriminal(){
+    console.log('Adding...');
+    $http
+      .post('/criminals', vm.newCriminal)
+      .then(function(response){
+        getCriminals();
+    });
+    vm.newCriminal = {};
+  }
 
+    // $http
+    // .post('/criminals', vm.newCriminal)
+    // .then(function someName(response){
+    //     vm.all.push(response.data.criminals);
+    //     vm.newCriminal = {};
+    // });
+  // }
+    function deleteCriminal(criminal){
+    $http
+      .delete("/criminals/" + criminal._id)
+      .then(function(response){
+        var index = vm.all.indexOf(criminal);
+        vm.all.splice(index, 1);
+    });
+  }
 }
