2540b9f7d4fc8335cb61b224067be45732616b97
Magkinney21 <magkinney21@gmail.com>
2017-04-13 18:16:09 -0400

    schema

---
+++ homework/sql_hmk/schema.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS stars;
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE stars (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  best_viewed VARCHAR(255),
+  description VARCHAR(300),
+  created_at DATE,
+  updated_at DATE
+          );
+
+CREATE TABLE user (
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+email VARCHAR(255),
+password_digest VARCHAR(255),
+created_at Date,
+updated_at Date,
+star_id INTERGER
+);
+
+CREATE TABLE horoscope (
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+user_id INTERGER,
+star_id INTERGER
+);
