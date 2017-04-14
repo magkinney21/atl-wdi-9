08e15fd9de857ae15d3c7772759ab6806db276f4
Magkinney21 <magkinney21@gmail.com>
2017-04-14 08:44:59 -0400

    new schema

---
+++ homework/sql_hmk/Query.sql
@@ -0,0 +1,8 @@
+Write out the SQL statement to grab all data from one of your tables.
+SELECT * FROM stars;
+Write out the SQL statement to grab all data from ALL of your tables.
+SELECT * FROM stars, users, horoscope;
+Write out the SQL statement to grab one column EACH from two of your tables.
+SELECT name FROM stars, users;
+Write out the SQL to update all values in one of your tables columns;
+UPDATE users SET name ="Jessica";
--- homework/sql_hmk/schema.sql
+++ homework/sql_hmk/schema.sql
@@ -1,15 +1,6 @@
 DROP TABLE IF EXISTS stars;
 DROP TABLE IF EXISTS user;
 
-CREATE TABLE stars (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
-  best_viewed VARCHAR(255),
-  description VARCHAR(300),
-  created_at DATE,
-  updated_at DATE
-          );
-
 CREATE TABLE user (
 id SERIAL PRIMARY KEY,
 name VARCHAR(255),
@@ -20,9 +11,19 @@ updated_at Date,
 star_id INTERGER
 );
 
+CREATE TABLE stars (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  best_viewed VARCHAR(255),
+  description VARCHAR(255),
+  created_at DATE,
+  updated_at DATE
+          );
+
 CREATE TABLE horoscope (
 id SERIAL PRIMARY KEY,
 name VARCHAR(255),
 user_id INTERGER,
 star_id INTERGER
 );
+
